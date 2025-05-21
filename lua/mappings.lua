f92d10708dd193c09c24a149a25b9def7d89f5f7
xvantz <xvantz@gmail.com>
2025-05-21 17:31:36 +0700

    add todo-comments
    lazy lock

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,21 +3,21 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
-map("n", "<C-z>", "u", { desc = "undo"})
-map("n", "<C-b>", function ()
+map("n", "<C-z>", "u", { desc = "undo" })
+map("n", "<C-b>", function()
   vim.lsp.buf.definition()
-end, { desc = "Go to defenition"})
+end, { desc = "Go to defenition" })
 map("n", "<leader>cx", function()
   require("nvchad.tabufline").closeAllBufs()
 end, { desc = "Close All Buffers" })
 map("t", "<C-q>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 map("n", "<C-q>", function()
-  local bufs = vim.fn.getbufinfo({ buflisted = 1})
+  local bufs = vim.fn.getbufinfo { buflisted = 1 }
 
   if #bufs <= 1 then
-    vim.cmd("q") -- последний буфер → закрываем Neovim
+    vim.cmd "q" -- последний буфер → закрываем Neovim
   else
-    vim.cmd("bd") -- иначе просто закрываем буфер
+    vim.cmd "bd" -- иначе просто закрываем буфер
   end
 end, { desc = "Close buffer", silent = true })
 map("n", ";", ":", { desc = "CMD enter command mode" })
@@ -36,9 +36,9 @@ map({ "n", "i" }, "<C-k>", function()
 end, { silent = true, noremap = true, desc = "toggle signature" })
 -- find mappings
 map("n", "<C-f>", function()
-  require('telescope.builtin').current_buffer_fuzzy_find()
+  require("telescope.builtin").current_buffer_fuzzy_find()
 end, { desc = "Поиск по текущему файлу (Telescope)" })
 map("n", "<leader>F", function()
-  require('telescope.builtin').live_grep()
+  require("telescope.builtin").live_grep()
 end, { desc = "Поиск по всем файлам (Telescope)" })
 map("n", "<leader>fp", "<cmd>Telescope projects<cr>", { desc = "Список проектов (project.nvim)" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,6 +5,29 @@ return {
     opts = require "configs.conform",
   },
   {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      explorer = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+  },
+  {
     "sindrets/diffview.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     cmd = { "DiffviewOpen", "DiffviewFileHistory" },
@@ -94,9 +117,9 @@ return {
         "typescript",
         "javascript",
       },
+      highlight = { enable = true },
     },
   },
-  { "hrsh7th/nvim-cmp" },
   { "kosayoda/nvim-lightbulb", event = "LspAttach", opts = { autocmd = { enabled = true } } },
   {
     "ray-x/lsp_signature.nvim",
