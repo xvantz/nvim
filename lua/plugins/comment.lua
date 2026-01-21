6c41c0b8a8ea8c5d636701668d17bd71622265c1
xvantz <xvantz@gmail.com>
2026-01-21 15:26:51 +0700

    fix: cleanup comment config

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,6 +1,7 @@
 return {
   "numToStr/Comment.nvim",
   dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+  event = { "BufReadPost", "BufNewFile" },
   keys = {
     { "gcc", mode = "n", desc = "Comment toggle current line" },
     { "gc", mode = { "n", "v" }, desc = "Comment toggle linewise" },
@@ -9,27 +10,7 @@ return {
   },
   config = function()
     require("Comment").setup({
-      padding = true,
-      sticky = true,
-      ignore = "^$",
-
       pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-
-      opleader = {
-        line = "<leader>c",
-        block = "<leader>b",
-      },
-      extra = {
-        above = "gcO",
-        below = "gco",
-        eol = "gcA",
-      },
-
-      toggler = { line = "gcc", block = "gbc" },
-      mappings = {
-        basic = true,
-        extra = true,
-      },
     })
 
     require("ts_context_commentstring").setup({
