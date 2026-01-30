89b841400f3f537af4e7045eceeb0b536bd5c527
xvantz <xvantz@gmail.com>
2026-01-30 23:03:02 +0700

    feat: add biomejs lint and lsp

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,7 +6,7 @@ return {
     {
       "<leader>f",
       function()
-        require("conform").format({ async = false })
+        require("conform").format({ async = false, timeout_ms = 3000 })
       end,
       desc = "Format Buffer",
     },
@@ -15,11 +15,12 @@ return {
     formatters_by_ft = {
       lua = { "stylua" },
       python = { "black", "isort" },
-      javascript = { "prettierd" },
-      typescript = { "prettierd" },
-      javascriptreact = { "prettierd" },
-      typescriptreact = { "prettierd" },
-      json = { "prettierd" },
+      javascript = { "biome", "prettierd", stop_after_first = true },
+      typescript = { "biome", "prettierd", stop_after_first = true },
+      javascriptreact = { "biome", "prettierd", stop_after_first = true },
+      typescriptreact = { "biome", "prettierd", stop_after_first = true },
+      json = { "biome", "prettierd", stop_after_first = true },
+      svelte = { "prettierd" },
       html = { "prettierd" },
       css = { "prettierd" },
       markdown = { "prettierd" },
@@ -27,15 +28,22 @@ return {
       rust = { "rustfmt" },
       go = { "gofmt" },
       sh = { "shfmt" },
-      svelte = { "prettierd" },
     },
     format_on_save = {
       timeout_ms = 3000,
       lsp_fallback = false,
     },
     formatters = {
-      prettier = {
-        prepend_args = { "--single-quote", "--jsx-single-quote" },
+      biome = {
+        condition = function(_, ctx)
+          return vim.fs.find({ "biome.json", "biome.jsonc" }, { upward = true, path = ctx.filename })[1]
+        end,
+      },
+      prettierd = {
+        condition = function(_, ctx)
+          local has_biome = vim.fs.find({ "biome.json", "biome.jsonc" }, { upward = true, path = ctx.filename })[1]
+          return not has_biome or vim.bo[ctx.buf].filetype == "svelte"
+        end,
       },
       stylua = {
         prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" },
--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -5,19 +5,41 @@ return {
     local lint = require("lint")
 
     lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
       python = { "flake8" },
       lua = { "selene" },
       markdown = { "markdownlint" },
       sh = { "shellcheck" },
     }
 
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    local function get_js_linters()
+      local root = vim.fs.find(
+        { "biome.json", "biome.jsonc", ".eslintrc", ".eslintrc.js", ".eslintrc.json", "eslint.config.js" },
+        { upward = true, path = vim.api.nvim_buf_get_name(0) }
+      )[1]
+      if not root then
+        return {}
+      end
+      local filename = vim.fn.fnamemodify(root, ":t")
+
+      if filename:find("biome") then
+        return { "biomejs" }
+      elseif filename:find("eslint") then
+        return { "eslint_d" }
+      end
+
+      return {}
+    end
+
+    vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
       callback = function()
-        require("lint").try_lint()
+        local ft = vim.bo.filetype
+        local js_ft = { "javascript", "typescript", "javascriptreact", "typescriptreact" }
+
+        if vim.tbl_contains(js_ft, ft) then
+          lint.try_lint(get_js_linters())
+        else
+          lint.try_lint()
+        end
       end,
     })
   end,
