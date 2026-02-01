8cb22842191bb82e4d116c604f1a08e435e647b9
xvantz <xvantz@gmail.com>
2026-02-01 19:17:38 +0700

    fix: delete nvim-treesitter-textobjects and update config for latest version

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,18 +3,36 @@ return {
   build = ":TSUpdate",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
+    -- "nvim-treesitter/nvim-treesitter-textobjects",
     "mason-org/mason.nvim",
   },
   config = function()
-    require("nvim-treesitter.configs").setup({
+    require("nvim-treesitter").setup({
       ensure_installed = {
-        "lua", "vim", "vimdoc", "query",
-        "javascript", "typescript", "tsx", "jsdoc",
-        "rust", "go", "python", "bash",
-        "html", "css", "json", "yaml", "toml",
-        "markdown", "markdown_inline", "regex",
-        "vue", "svelte", "prisma", "graphql",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "javascript",
+        "typescript",
+        "tsx",
+        "jsdoc",
+        "rust",
+        "go",
+        "python",
+        "bash",
+        "html",
+        "css",
+        "json",
+        "yaml",
+        "toml",
+        "markdown",
+        "markdown_inline",
+        "regex",
+        "vue",
+        "svelte",
+        "prisma",
+        "graphql",
       },
 
       sync_install = false,
@@ -72,18 +90,5 @@ return {
         },
       },
     })
-
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    for _, parser in pairs(parser_config) do
-      if parser.install_info and parser.install_info.url:find("mason") then
-        local ok, mason = pcall(require, "mason-registry")
-        if ok and mason.has_package then
-          local pkg_name = "tree-sitter-" .. parser.name
-          if mason.has_package(pkg_name) and not mason.is_installed(pkg_name) then
-            mason.get_package(pkg_name):install()
-          end
-        end
-      end
-    end
   end,
 }
