6578cebdb188cc68a5847f4d8928cf8660520890
xvantz <xvantz@gmail.com>
2026-02-04 00:28:45 +0700

    fix: fix todo comments crash

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -2,6 +2,7 @@ return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   event = { "BufReadPre", "BufNewFile" },
+  -- TODO
   keys = {
     { "<leader>ft", "<cmd>TodoTelescope<CR>", desc = "Find TODOs (Telescope)" },
     { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "TODOs in Trouble" },
@@ -21,8 +22,6 @@ return {
     },
   },
   opts = {
-    signs = true,
-    sign_priority = 8,
     keywords = {
       FIX = { icon = " ", color = "#f38ba8", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
       TODO = { icon = " ", color = "#89b4fa", alt = { "WIP" } },
@@ -32,41 +31,14 @@ return {
       NOTE = { icon = "󰍩 ", color = "#94e2d5", alt = { "INFO", "HINT" } },
       TEST = { icon = "⏲ ", color = "#a6e3a1", alt = { "TESTING", "PASSED", "FAILED" } },
     },
-    gui_style = {
-      fg = "NONE",
-      bg = "BOLD",
-    },
-    merge_keywords = true,
     highlight = {
-      multiline = true,
-      multiline_pattern = "^.",
-      multiline_context = 10,
-      before = "fg",
-      keyword = "wide_bg",
-      after = "fg",
-      pattern = [[.*<(KEYWORDS)\s*(\([^\)]*\))?:?]],
-      comments_only = true,
-      exclude = {},
-    },
-    colors = {
-      error = { "DiagnosticError", "f38ba8" },
-      warning = { "DiagnosticWarn", "f9e2af" },
-      info = { "DiagnosticInfo", "94e2d5" },
-      hint = { "DiagnosticHint", "89b4fa" },
-      default = { "Identifier", "cba6f7" },
-      test = { "Identifier", "a6e3a1" },
+      multiline = false,
+      keyword = "bg",
+      pattern = [[<(KEYWORDS)]],
     },
     search = {
       command = "rg",
-      args = {
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--hidden",
-      },
-      pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]],
+      pattern = [[\b(KEYWORDS)\b]],
     },
   },
 }
