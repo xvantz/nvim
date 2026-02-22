bf8000ddd7e3d11efe0b99073aeb441803998257
Ivan R. <xvantz@gmail.com>
2026-02-22 20:28:24 +0700

    feat: delete auto install lsp, linters, formaters

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -24,47 +24,9 @@ return {
     dependencies = { "mason-org/mason.nvim" },
     event = { "BufReadPre", "BufNewFile" },
     opts = {
-      ensure_installed = {
-        "lua_ls",
-        "vtsls",
-        "rust_analyzer",
-        "gopls",
-        "html",
-        "cssls",
-        "jsonls",
-        "bashls",
-        "marksman",
-        "tailwindcss",
-      },
+      ensure_installed = {},
       automatic_installation = true,
       automatic_enable = false,
     },
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = { "mason-org/mason.nvim" },
-    event = { "VeryLazy" },
-    config = function()
-      require("mason-tool-installer").setup({
-        ensure_installed = {
-          -- Formatters
-          "prettierd",
-          "black",
-          "isort",
-          "stylua",
-          "gofmt",
-          "rustfmt",
-          "shfmt",
-          -- Linters
-          "eslint_d",
-          "flake8",
-          "selene",
-          "shellcheck",
-          "markdownlint",
-        },
-        auto_update = true,
-        run_on_start = true,
-      })
-    end,
   },
 }
